0a8a81064d9dc164a26aea6970bc93fba1500eb1
Kamil <bienius.234@gmail.com>
2026-06-30 20:00:53 +0200

    Init repo

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+
+const app = express();
+
+app.listen(8000, () => {
+  console.log('Server is running on port: 8000');
+});
+
+// connect to DB
+
+// add middleware
+
+//serve static files from react.app
+
+//add routes
